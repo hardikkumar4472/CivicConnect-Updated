b77a5ddd7f12cdff863827cdfbc7b6a031cb19a2
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-08-20 13:37:38 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 const connectDB=async()=>{
     try{
-        await mongoose.connect(process.env.mongo_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB is connected Successfully");
     }
     catch (err){
