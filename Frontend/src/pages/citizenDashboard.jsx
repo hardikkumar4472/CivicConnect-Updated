509d587864fcda5e8a3a300fe41715a404147535
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-08-20 15:23:27 +0530

    Update citizenDashboard.jsx

--- Frontend/src/pages/citizenDashboard.jsx
+++ Frontend/src/pages/citizenDashboard.jsx
@@ -35,7 +35,7 @@ export default function CitizenDashboard() {
     if (closedIssues.length > 0) {
       try {
         const response = await axios.get(
-          "http://localhost:5000/api/feedback/batch",
+          "https://civicconnect-backend.onrender.com/api/feedback/batch",
           {
             headers: { Authorization: `Bearer ${token}` },
             params: {
@@ -69,10 +69,10 @@ export default function CitizenDashboard() {
       }
 
       const [citizenRes, issuesRes] = await Promise.all([
-        axios.get("http://localhost:5000/api/citizen/me", {
+        axios.get("https://civicconnect-backend.onrender.com/api/citizen/me", {
           headers: { Authorization: `Bearer ${token}` }
         }),
-        axios.get("http://localhost:5000/api/issues/my", {
+        axios.get("https://civicconnect-backend.onrender.com/api/issues/my", {
           headers: { Authorization: `Bearer ${token}` }
         })
       ]);
@@ -124,7 +124,7 @@ export default function CitizenDashboard() {
     try {
       const token = localStorage.getItem("token");
       const response = await axios.post(
-        "http://localhost:5000/api/issues/report",
+        "https://civicconnect-backend.onrender.com/api/issues/report",
         newIssueData,
         { headers: { Authorization: `Bearer ${token}` } }
       );
@@ -142,7 +142,7 @@ export default function CitizenDashboard() {
       setExporting(true);
       const token = localStorage.getItem("token");
       const response = await axios.get(
-        "http://localhost:5000/api/issues/export-issues",
+        "https://civicconnect-backend.onrender.com/api/issues/export-issues",
         {
           headers: { Authorization: `Bearer ${token}` },
           responseType: "blob"
@@ -181,7 +181,7 @@ export default function CitizenDashboard() {
   try {
      const token = localStorage.getItem("token");
     await axios.post(
-      "http://localhost:5000/api/feedback/submit",
+      "https://civicconnect-backend.onrender.com/api/feedback/submit",
       { issueId, rating, comment },
       { headers: { Authorization: `Bearer ${token}` } }
     );
