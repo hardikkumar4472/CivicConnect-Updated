7a866f005495579f97c9e7eab4e7306438547c7d
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-08-20 14:42:14 +0530

    Delete Frontend directory

--- Frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:5000",
-  withCredentials: true, // important if backend sends cookies
-});
-
-export default api;
