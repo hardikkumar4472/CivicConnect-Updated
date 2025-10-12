3e1287d290cb52c41a6dd9fc793dcf7f2d7f672e
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-10-12 10:41:52 +0530

    Delete Backend directory

--- Backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { registerAdmin, loginAdmin } from '../controllers/authController.js';
-
-import { registerSectorHead, loginSectorHead} from '../controllers/sectorHeadController.js';
-const router = express.Router();
-router.post('/admin', registerAdmin);
-router.post('/login', loginAdmin);
-export default router;
--- Backend/routes/requestRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import authCitizen from '../middleware/authCitizen.js';
-import { createRequest, getMyRequests } from "../controllers/requestController.js";
-const router = express.Router();
-router.post("/create", authCitizen, createRequest);
-router.get("/my-requests", authCitizen, getMyRequests);
-
-export default router;
