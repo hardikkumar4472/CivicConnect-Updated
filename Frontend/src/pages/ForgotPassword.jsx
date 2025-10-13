506aa408616fcb697d446c1d86b1672c75eff5cc
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-10-13 09:53:19 +0530

    Update ForgotPassword.jsx

--- Frontend/src/pages/ForgotPassword.jsx
+++ Frontend/src/pages/ForgotPassword.jsx
@@ -33,7 +33,7 @@ const ForgotPassword = () => {
     
     try {
       const response = await axios.post(
-        'https://civicconnect-backend.onrender.com/api/citizen/forgot-password', 
+        'https://civic-connect-vercel-hosted.vercel.app/api/citizen/forgot-password', 
         { email }
       );
       
