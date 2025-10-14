c55381811c22cd996097f32dd45e6538ee7585ef
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-10-14 23:09:23 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -31,7 +31,7 @@ app.use(cors({
   credentials: true,
 }));
 
-// app.options("*", cors());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/sector-head', sectorHeadRoutes);
 app.use('/api/citizen', citizenRoutes);
